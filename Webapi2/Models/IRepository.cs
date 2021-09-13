f30b1c20c430f0d390f9c51503934499d2be9fb8
Basavaraja Chalagere <Basavaraja.Cn@sigma.se>
2021-09-13 12:39:35 +0200

    my api code for initial push

---
+++ Webapi2/Models/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webapi2.Models
+{
+   public interface IRepository
+    {
+        IEnumerable<Reservation> reservations { get; }
+        Reservation this[int Id] { get;}
+        Reservation AddReservation(Reservation reservation);
+        Reservation UpdateReservation(Reservation reservation);
+        void DeleteReservation(int Id);
+    }
+}
